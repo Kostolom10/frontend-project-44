b54e8f540bd65b71463f94b3bf53691481bb1946
Ivan Kulakov <mister.kostolom10@yandex.ru>
2024-03-29 18:14:31 +0300

    fix

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
-import welcome from '../src/cli.js';
+import welcome from "../src/cli.js";
 
 welcome();
+
+export default function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
